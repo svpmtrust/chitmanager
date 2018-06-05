f9c5ccbbd3d89beedc520ff557375476c76c483b
NARAYANA SHANMUKHA VENKAT <narayana.svenkat.cse17@itbhu.ac.in>
2018-06-05 12:16:24 +0530

    reverted to old code

--- src/chits/settings.py
+++ src/chits/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 if os.environ.get('SQLITE','False') == 'True':
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'chit_data.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '..', 'chit_data.sqlite3'),
     }
 
 if 'DATABASE_URL' in os.environ:
